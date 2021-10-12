20d3830a977eacade9868155d96a2978b9aa982d
Irina-coud <irinatobolich313@gmail.com>
2021-10-12 20:40:16 +0300

    feat: add remove/min/max

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -74,19 +74,74 @@ module.exports = class BinarySearchTree {
 
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.node = removeNode(this.node, data)
+
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+
+        node.right = removeNode(node.right, minFromRight.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.node) {
+      return;
+    }
+
+    let node = this.node;
+    while (node.left) {
+      node = node.left
+    }
+
+    return node.data
+
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.node) {
+      return;
+    }
+
+    let node = this.node;
+    while (node.right) {
+      node = node.right
+    }
+
+    return node.data
   }
 
 }
